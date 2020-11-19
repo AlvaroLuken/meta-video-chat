89a7f0d55de9f2aed77d61289f78dde123baafd6
Daniel Nolan <DanNolan99@gmail.com>
2020-11-18 23:28:44 -0500

    some changes

--- public/script.js
+++ public/script.js
@@ -1,80 +1,56 @@
-const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-  host: '/',
-  port: '3001'
-})
-const myVideo = document.createElement('video')
-myVideo.muted = true
-const peers = {}
+const videoGrid = document.getElementById('video-grid');
+const myVideo = document.createElement('video');
 
-console.log(ethereum.isConnected())
+ethereum.enable().then(() => {
+  const id = ethereum.selectedAddress + Date.now();
 
+  const peer = new Peer(id, {
+    host: window.location.hostname,
+    port: window.location.port,
+    path: '/peerjs/myapp'
+  });
 
-if(ethereum.isConnected()) {
-  navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-  }).then(stream => {
-    console.log("HEY?");
-    addVideoStream(myVideo, stream)
+  peer.on('connection', function(conn) {
+    const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    getUserMedia({video: true, audio: true}, function(stream) {
+      const call = peer.call(conn.peer, stream);
+      call.on('stream', function(stream) {
+        const video = document.createElement("video");
+        addVideoStream(video, stream)
+      });
+    }, function(err) {
+      console.log('Failed to get local stream' ,err);
+    });
+  });
 
-    myPeer.on('call', call => {
-      call.answer(stream)
-      const video = document.createElement('video')
+  peer.on('call', function(call) {
+    const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    getUserMedia({video: true, audio: true}, function(stream) {
+      call.answer(stream); // Answer the call with an A/V stream.
+      call.on('stream', function(stream) {
+        const video = document.createElement("video");
+        addVideoStream(video, stream)
+      });
+    }, function(err) {
+      console.log('Failed to get local stream' ,err);
+    });
+  });
 
-      call.on('stream', userVideoStream => {
-        addVideoStream(video, userVideoStream)
-      })
-    })
-
-    socket.on('user-connected', userId => {
-      connectToNewUser(userId, stream)
-    })
-  })
-}
-
-
-socket.on('user-disconnected', userId => {
-  if (peers[userId]) peers[userId].close()
-})
-
-myPeer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id)
-})
-
-function connectToNewUser(userId, stream) {
-  const call = myPeer.call(userId, stream)
-  const video = document.createElement('video')
-  call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
-  })
-  call.on('close', () => {
-    video.remove()
-  })
-
-  peers[userId] = call
-}
+  peer.on('open', function(){
+    axios.get("/friends").then((friends) => {
+      friends.forEach((friend) => {
+        if(friend !== id) {
+          peer.connect(friend);
+        }
+      });
+    });
+  });
+});
 
 function addVideoStream(video, stream) {
-  video.srcObject = stream
+  video.srcObject = stream;
   video.addEventListener('loadedmetadata', () => {
-    video.play()
+    video.play();
   })
-  let t = document.createTextNode('HEYY');
-  video.append("EHEHEHE");
-  videoGrid.append(video)
-  
+  videoGrid.append(video);
 }
-
-
-
-
-
-
-
-
-
-
-
-
